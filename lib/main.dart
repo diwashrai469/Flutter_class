512408581a3d4bc028c46dd19146e5e796213aca
Diwash Rai <raidiwash30@gmail.com>
2022-12-14 08:26:01 +0545

    new project

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: homepage(),
+  ));
+}
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        leading: Builder(builder: (context) {
+          return ElevatedButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            child: Icon(
+              Icons.close,
+            ),
+          );
+        }),
+        centerTitle: true,
+        title: Text(
+          "about page",
+        ),
+        actions: [
+          ElevatedButton(onPressed: () {}, child: Icon(Icons.card_giftcard)),
+          ElevatedButton(
+              onPressed: () {}, child: Icon(Icons.card_travel_rounded)),
+        ],
+      ),
+      body: Center(child: Text("Hello world")),
+      drawer: Drawer(
+        child: ListView(children: [
+          Icon(Icons.image_aspect_ratio_outlined),
+          ElevatedButton(onPressed: () {}, child: Icon(Icons.card_giftcard)),
+          ElevatedButton(
+              onPressed: () {}, child: Icon(Icons.card_travel_rounded)),
+          Divider(),
+          Card(
+            child: ListTile(title: Text("menu")),
+          ),
+          Card(
+            child: ListTile(title: Text("menu")),
+          ),
+          Card(
+            child: ListTile(title: Text("menu")),
+          )
+        ]),
+      ),
+    ));
+  }
+}
